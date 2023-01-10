42a21f8e1bcd06a60c4af08c65615b5991096b64
Simon Hova <shova@nerdsthatcare.com>
2023-01-10 09:14:40 -0500

    Create Check Archive Size Progress.ps1
    Initial commit. Don't know if it's complete.

---
+++ Check Archive Size Progress.ps1
@@ -0,0 +1,17 @@
+﻿$UserName = Read-Host -Prompt "Enter the email address of the mailbox you want to check"
+
+While ($true)
+{
+$MailBox=Get-MailboxStatistics -Identity $UserName
+$Archive=Get-MailboxStatistics -Identity $UserName -Archive
+
+Write-Progress -Id 3 -Activity "Mailbox Size" -Status $MailBox.TotalItemSize -PercentComplete -1
+Write-Progress -Id 1 -Activity "Mailbox Count" -Status $MailBox.ItemCount -PercentComplete -1 
+
+Write-Progress -Id 4 -Activity "Archive Size" -Status $Archive.TotalItemSize -PercentComplete -1
+Write-Progress -Id 2 -Activity "Archive Count" -Status $Archive.ItemCount -PercentComplete -1
+
+
+# Write-Progress(Get-MailboxStatistics -Identity "dashen@dashdesign.net" -Archive | Select TotalItemSize, ItemCount)
+Start-Sleep -Milliseconds 5000
+}
