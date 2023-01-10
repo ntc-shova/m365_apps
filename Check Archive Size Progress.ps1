56680845c9368fccab8a408fa6ce52510df84500
Simon Hova <shova@nerdsthatcare.com>
2023-01-10 09:23:17 -0500

    Update Check Archive Size Progress.ps1
    Added a trim to the input.
    Sanitizing my code.

--- Check Archive Size Progress.ps1
+++ Check Archive Size Progress.ps1
@@ -1,5 +1,5 @@
 ﻿Connect-ExchangeOnline
-$UserName = Read-Host -Prompt "Enter the email address of the mailbox you want to check"
+$UserName = Read-Host -Prompt "Enter the email address of the mailbox you want to check".Trim()
 
 While ($true)
 {
@@ -13,6 +13,6 @@ Write-Progress -Id 4 -Activity "Archive Size" -Status $Archive.TotalItemSize -Pe
 Write-Progress -Id 2 -Activity "Archive Count" -Status $Archive.ItemCount -PercentComplete -1
 
 
-# Write-Progress(Get-MailboxStatistics -Identity "dashen@dashdesign.net" -Archive | Select TotalItemSize, ItemCount)
+# Write-Progress(Get-MailboxStatistics -Identity $UserName -Archive | Select TotalItemSize, ItemCount)
 Start-Sleep -Milliseconds 5000
 }
