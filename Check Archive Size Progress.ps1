d5de20bf442efd928a98e682bfc117bb296e3c8a
Simon Hova <shova@nerdsthatcare.com>
2023-01-13 09:23:44 -0500

    Update Check Archive Size Progress.ps1
    Now working!

--- Check Archive Size Progress.ps1
+++ Check Archive Size Progress.ps1
@@ -8,10 +8,13 @@ param(
 )
 Connect-ExchangeOnline
 
+$User = $UserName[0]
+clear
+
 While ($true)
 {
-$MailBox=Get-EXOMailboxStatistics -Identity $UserName
-$Archive=Get-EXOMailboxStatistics -Identity $UserName -Archive
+$MailBox=Get-EXOMailboxStatistics -Identity $User
+$Archive=Get-EXOMailboxStatistics -Identity $User -Archive
 
 Write-Progress -Id 3 -Activity "Mailbox Size" -Status $MailBox.TotalItemSize -PercentComplete -1
 Write-Progress -Id 1 -Activity "Mailbox Count" -Status $MailBox.ItemCount -PercentComplete -1 
@@ -20,6 +23,6 @@ Write-Progress -Id 4 -Activity "Archive Size" -Status $Archive.TotalItemSize -Pe
 Write-Progress -Id 2 -Activity "Archive Count" -Status $Archive.ItemCount -PercentComplete -1
 
 
-# Write-Progress(Get-EXOMailboxStatistics -Identity $UserName -Archive | Select TotalItemSize, ItemCount)
+# Write-Progress(Get-EXOMailboxStatistics -Identity $User -Archive | Select TotalItemSize, ItemCount)
 Start-Sleep -Milliseconds 5000
 }
