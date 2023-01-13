ca5dcd27f87707be4cde93e05c44b4d66a917d5b
Simon Hova <shova@nerdsthatcare.com>
2023-01-13 16:10:12 -0500

    Update Check Archive Size Progress.ps1
    Added a line that displays the mailbox that we are checking.

--- Check Archive Size Progress.ps1
+++ Check Archive Size Progress.ps1
@@ -16,11 +16,13 @@ While ($true)
 $MailBox=Get-EXOMailboxStatistics -Identity $User
 $Archive=Get-EXOMailboxStatistics -Identity $User -Archive
 
-Write-Progress -Id 3 -Activity "Mailbox Size" -Status $MailBox.TotalItemSize -PercentComplete -1
-Write-Progress -Id 1 -Activity "Mailbox Count" -Status $MailBox.ItemCount -PercentComplete -1 
+Write-Output "Checking mailbox $User"
 
-Write-Progress -Id 4 -Activity "Archive Size" -Status $Archive.TotalItemSize -PercentComplete -1
-Write-Progress -Id 2 -Activity "Archive Count" -Status $Archive.ItemCount -PercentComplete -1
+Write-Progress -Id 4 -Activity "Mailbox Size" -Status $MailBox.TotalItemSize -PercentComplete -1
+Write-Progress -Id 2 -Activity "Mailbox Count" -Status $MailBox.ItemCount -PercentComplete -1 
+
+Write-Progress -Id 5 -Activity "Archive Size" -Status $Archive.TotalItemSize -PercentComplete -1
+Write-Progress -Id 3 -Activity "Archive Count" -Status $Archive.ItemCount -PercentComplete -1
 
 
 # Write-Progress(Get-EXOMailboxStatistics -Identity $User -Archive | Select TotalItemSize, ItemCount)
